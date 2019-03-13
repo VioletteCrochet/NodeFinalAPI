26412daa1af91c5fa00eb4fe55cb0c49ff025c95
Crochet Olivier <olivier.crochet@ynov.com>
2019-03-13 14:33:49 +0100

    demo racine

--- server.js
+++ server.js
@@ -11,7 +11,22 @@ const app = express();
 
 app.use(bodyParser.json());
 
-app.post('/character', (req, res) => {
+// app.post('/character', (req, res) => {
+//     let newCharacter = new character( {
+//         name: req.body.name,
+//         classe: req.body.classe,
+//         level: req.body.level
+//     });
+//     newCharacter.save().then( character => {
+//         res.send(character);
+//     }).catch((err) => {
+//         res.status(500).send(err);
+//     });
+// });
+
+app.post('/:route', (req, res) => {
+    const {route} = req.params;
+    if (route === 'character') {
     let newCharacter = new character( {
         name: req.body.name,
         classe: req.body.classe,
@@ -22,9 +37,32 @@ app.post('/character', (req, res) => {
     }).catch((err) => {
         res.status(500).send(err);
     });
+    } else if (route === 'classe') {
+        let newClasse = new classe( {
+            name: req.body.name,
+            spells: req.body.classe,
+            level: req.body.level
+        });
+        newClasse.save().then( classe => {
+            res.send(classe);
+        }).catch((err) => {
+            res.status(500).send(err);
+        });
+    } else if (route === 'alignement') {
+        let newAlignement = new alignement( {
+            name: req.body.name,
+            cite: req.body.cite,
+            level: req.body.level
+        });
+        newAlignement.save().then( alignement => {
+            res.send(alignement);
+        }).catch((err) => {
+            res.status(500).send(err);
+        });
+    }
 });
 
-app.delete('/character/:id', (req,res) => { 
+app.delete('/:route/:id', (req,res) => { 
     const {id} = req.params;
     if (!ObjectID.isValid(id)) {
         res.status(400).send()
@@ -39,6 +77,23 @@ app.delete('/character/:id', (req,res) => {
     })
 });
 
+
+
+// app.delete('/character/:id', (req,res) => { 
+//     const {id} = req.params;
+//     if (!ObjectID.isValid(id)) {
+//         res.status(400).send()
+//     }
+//     character.findByIdAndDelete(id).then((character) => {
+//         if (!character) {
+//             res.status(404).send()
+//         }
+//         res.send("deleted");
+//     }).catch((err) => {
+//         res.status(500).send()
+//     })
+// });
+
 app.put('/character/:id', (req, res) => {         
     const { id } = req.params;       
     if (!ObjectID.isValid(id)) {             
@@ -50,7 +105,9 @@ app.put('/character/:id', (req, res) => {
             } else { 
                 character.findById(id).then(newCharacter => {           
                     res.json(newCharacter).send();
-                })                
+                }).catch(err => {                 
+                    res.status(500).send();             
+                });                
             }             
         }).catch(err => {                 
             res.status(500).send();             

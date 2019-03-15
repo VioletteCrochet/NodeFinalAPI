fd1e359c0b92113c10576b3ec45f94394cf2c2b4
VADILLO Matthieu <mat.vadillo@orange.fr>
2019-03-15 13:20:24 +0100

    class methods calls

--- server.js
+++ server.js
@@ -27,92 +27,34 @@ app.post('/character', (req, res) => {
     postCharacter(req,res)
 });
 
+// DELETE METHOD CHARACTER
 app.delete('/character/:id', (req,res) => {
     deleteCharacter(req, res)
 });
 
+// PUT METHOD CHARACTER
 app.put('/character/:id', (req, res) => {
     putCharacter(req, res)
 });
 
-// POST METHOD ALIGNEMENT
-app.post('/alignement', (req, res) => {
-    postAlignement( req,res)
+// GET METHOD CLASSE
+app.get('/classe', (req, res) => {
+    getClasse(req, res)
 });
 
 // POST METHOD CLASSE
 app.post('/classe', (req, res) => {
-    let newClasse = new classe( {
-        className: req.body.className,
-        spells: req.body.spells,
-        role: req.body.role
-    });
-    newClasse.save().then( classe => {
-        res.send(classe);
-    }).catch((err) => {
-        res.status(500).send(err);
-    });
-});
-
-// DELETE METHOD CHARACTER
-
-// DELETE METHOD ALIGNEMENT
-app.delete('/alignement/:id', (req,res) => {
-    deleteAlignement (req,res)
-    
+    postClasse(req, res)
 });
 
 // DELETE METHOD CLASSE
 app.delete('/classe/:id', (req,res) => {
-    const {id} = req.params;
-    if (!ObjectID.isValid(id)) {
-        res.status(400).send()
-    }
-    classe.findByIdAndDelete(id).then((classe) => {
-        if (!classe) {
-            res.status(404).send()
-        }
-        res.send("deleted");
-    }).catch((err) => {
-        res.status(500).send()
-    })
-});
-
-// PUT METHOD CHARACTER
-
-
-// PUT METHOD ALIGNEMENT
-app.put('/alignement/:id', (req, res) => {
-    putAlignement(req,res)
+    deleteClasse(req,res)
 });
 
 // PUT METHOD CLASSE
 app.put('/classe/:id', (req, res) => {
-    const { id } = req.params;
-    if (!ObjectID.isValid(id)) {
-        res.status(400).send('invalid ID');
-    } else {
-        classe.findByIdAndUpdate(id, { $set: req.body }).then(oldClasse => {
-            if (!oldClasse) {
-                res.status(404).send();
-            } else {
-                classe.findById(id).then(newClasse => {
-                    res.json(newClasse).send();
-                })
-            }
-        }).catch(err => {
-            res.status(500).send();
-        });
-    }
-});
-
-// GET METHOD CHARACTER
-app.get('/character', (req, res) => {
-    character.find().then(listOfCharacter => {
-        res.json(listOfCharacter)
-    }).catch(err => {
-        res.status(500).send()
-    });
+    putClasse(req,res)
 });
 
 // GET METHOD ALIGNEMENT
@@ -120,17 +62,22 @@ app.get('/alignement', (req, res) => {
     getAlignement(req,res)
 });
 
-// GET METHOD CLASSE
-app.get('/classe', (req, res) => {
-    classe.find().then(listOfClasse => {
-        res.json(listOfClasse)
-    }).catch(err => {
-        res.status(500).send()
-    });
+// POST METHOD ALIGNEMENT
+app.post('/alignement', (req, res) => {
+    postAlignement( req,res)
+});
+
+// DELETE METHOD ALIGNEMENT
+app.delete('/alignement/:id', (req,res) => {
+    deleteAlignement (req,res)
+});
+
+// PUT METHOD ALIGNEMENT
+app.put('/alignement/:id', (req, res) => {
+    putAlignement(req,res)
 });
 
 // LISTEN
-
 app.listen(port, () => {
     console.log('Started on port: ' + port )
 })

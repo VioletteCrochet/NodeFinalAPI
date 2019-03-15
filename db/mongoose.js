391775098b47f1624fbd29e733e171f40dd1b1aa
Crochet Olivier <olivier.crochet@ynov.com>
2019-03-15 23:02:44 +0100

    connection with diferet users done

--- db/mongoose.js
+++ db/mongoose.js
@@ -1,13 +1,19 @@
 //import mongoose module
 const mongoose = require('mongoose');
+const {argv} = require('yargs');
 
-const {user} = require('../server.js');
-const {password} = require('../server.js')
+// const user = require('../server.js').user;
+// const password = require('../server.js').password;
+
+const user = argv._[0];
+const password = argv._[1];
 
 //define use of promises
 mongoose.Promise = global.Promise;
 
 //define database connection
+console.log(user)
+console.log(password)
 mongoose.connect(`mongodb+srv://${user}:${password}@cluster0-3n0v9.gcp.mongodb.net/dofusAPI?retryWrites=true`, { useNewUrlParser: true });
 
 //expose module
--- server.js
+++ server.js
@@ -3,19 +3,12 @@ const {argv} = require('yargs');
 const { app } = require('./routes.js')
 
 //define port based on input args
-const inputPort = argv._[0]
+const inputPort = argv._[2];
 const port = (inputPort && !isNaN(inputPort) && (inputPort > 0 && inputPort % 1 === 0)) ? inputPort : 3000;
 
 //define user and password based on args
-const user = argv._[1];
-const password = argv._[2];
 
 // Start app on desired port
 app.listen(port, () => {
     console.log('Started on port: ' + port )
 })
-
-module.exports = {
-    user,
-    password
-}

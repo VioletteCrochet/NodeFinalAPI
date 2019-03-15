0a53df4abc623bdd5338634796b43e488925a3ff
Crochet Olivier <olivier.crochet@ynov.com>
2019-03-15 17:30:56 +0100

    added definition of users

--- db/mongoose.js
+++ db/mongoose.js
@@ -1,10 +1,14 @@
 //import mongoose module
 const mongoose = require('mongoose');
 
+const {user} = require('../server.js');
+const {password} = require('../server.js')
+
 //define use of promises
 mongoose.Promise = global.Promise;
+
 //define database connection
-mongoose.connect('mongodb+srv://oliviercrochet:mathéolecon@cluster0-3n0v9.gcp.mongodb.net/dofusAPI?retryWrites=true', { useNewUrlParser: true });
+mongoose.connect(`mongodb+srv://${user}:${password}@cluster0-3n0v9.gcp.mongodb.net/dofusAPI?retryWrites=true`, { useNewUrlParser: true });
 
 //expose module
 module.exports = {
--- server.js
+++ server.js
@@ -6,7 +6,16 @@ const { app } = require('./routes.js')
 const inputPort = argv._[0]
 const port = (inputPort && !isNaN(inputPort) && (inputPort > 0 && inputPort % 1 === 0)) ? inputPort : 3000;
 
+//define user and password based on args
+const user = argv._[1];
+const password = argv._[2];
+
 // Start app on desired port
 app.listen(port, () => {
     console.log('Started on port: ' + port )
 })
+
+module.exports = {
+    user,
+    password
+}
